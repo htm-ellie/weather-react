9b5f0d43231c8c5c4e57587a656285c105eb4d6c
htm-ellie <html.elitt@gmail.com>
2026-03-13 12:24:59 -0400

    updated links

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ export default function App() {
         <footer className="mt-3">
           Coded by Ellie Litt and{" "}
           <a href="https://github.com/htm-ellie/weather-react" target="_blank">
-            open-sourced on GitHub
+            open-sourced on GitHub{" "}
+          </a>
+          , hosted on{" "}
+          <a href="https://weather-react-ellie.netlify.app/" target="_blank">
+            Netlify
           </a>
         </footer>
       </div>
